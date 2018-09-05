72012cb169cacacff921168fad11fb47e36fa2c0
kence <568034555@qq.com>
2018-09-05 11:51:02 +0800

    开始添加前端框架

--- src/main/java/com/finance/controller/ActionController.java
+++ src/main/java/com/finance/controller/ActionController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
-@RequestMapping("/finance")
+
 public class ActionController {
     @RequestMapping(value={"/","/testHtml"})
     public String connectTestHtml(){
-        return "index";
+        return "main";
     }
 
     @ResponseBody
