4a44fa34c94bce8814e97ebc2fc01b396bb4d399
kence <568034555@qq.com>
2018-09-11 12:31:18 +0800

    新增shiro

---
+++ src/main/java/com/finance/config/ShiroConfig.java
@@ -0,0 +1,33 @@
+package com.finance.config;
+
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+
+    /**
+     * SecurityManager提供初始化ShiroFilterFactoryBean的时候使用
+     *
+     * */
+    @Bean
+    public SecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        // 设置realm.
+        securityManager.setRealm(myRealm());
+        return securityManager;
+    }
+
+    /**
+     * 身份认证realm; (这个需要自己写，账号密码校验；权限等)
+     *
+     * @return
+     */
+    @Bean
+    public MyRealm myRealm() {
+        MyRealm myRealm = new MyRealm();
+        return myRealm;
+    }
+}
